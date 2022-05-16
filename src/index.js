fec1f224b92224352bc63bee10cb269ed91f7d0a
Nerissa Dorlus <nerissadorlus@Tdas-Pro-3.local>
2022-05-16 11:58:05 -0400

    created more testing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+// returns the sum if a> b, or the difference otherwise
+
 const sum = (a, b) => {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "❌ Bad Input";
+  }
+  if (a < b) {
+    return b - a;
+  }
   return a + b;
 };
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,40 @@
 import sut from "../src/index.js";
 
-test("should add 2 numbers", () => {
-  //arrange
-  const c = 2;
-  const d = 3;
-  const expect = 5;
+describe("The test forr sum", () => {
+  fit("should add 2 numbers", () => {
+    //arrange
+    const c = 3;
+    const d = 5;
+    const expected = 2;
 
-  //act
-  const actual = sut(c, d);
+    //act
+    const actual = sut(c, d);
 
-  //assert
-  expect(actual).toBe(expected);
+    //assert
+    expect(actual).toBe(expected);
+  });
+  it("should add 2 numbers", () => {
+    //arrange
+    const c = 5;
+    const d = 3;
+    const expected = 8;
+
+    //act
+    const actual = sut(c, d);
+
+    //assert
+    expect(actual).toBe(expected);
+  });
+  it("should fail with a string input", () => {
+    //arange
+    const a = "str",
+      b = 5,
+      expected = "❌ Bad Input";
+
+    //Act
+    const actual = sut(a, b);
+
+    //assert
+    expect(actual).toBe(expected);
+  });
 });
